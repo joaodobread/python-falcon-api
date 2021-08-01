39516c2f0a535e7757a019d06b2e194ef699371b
João Victor <joaovictorslf@gmail.com>
2021-08-01 19:07:59 -0300

    feat: Added a jwt encoder

---
+++ main.py
@@ -0,0 +1,5 @@
+from dotenv import find_dotenv, load_dotenv
+
+from src.security.jwt_service import JwtService
+
+load_dotenv(find_dotenv())
---
+++ src/security/jwt_service.py
@@ -0,0 +1,42 @@
+import os
+from jwt import encode, decode
+
+
+class JwtService:
+    __instance = None
+
+    def __init__(self):
+        self.__secret = os.getenv('SECRET_KEY')
+
+    def sign(self, payload):
+        """Encode a payload to a jwt
+
+        Args:
+            payload (JSON): any payload to be transformed to jwt token
+
+        Returns:
+            str: jwt token
+        """
+        return encode(payload, self.__secret, algorithm="HS256")
+
+    def decode(self, jwt):
+        """Decode a JWT token
+
+        Args:
+            jwt (str): encoded jwt
+
+        Returns:
+            JSON: decoded jwt token
+        """
+        return decode(jwt, self.__secret, algorithms=["HS256"])
+
+    @classmethod
+    def get_instance(cls):
+        """
+        Returns:
+            JwtService: return a instance of JwtService
+        """
+        if cls.__instance is not None:
+            return cls.__instance
+        cls.__instance = cls()
+        return cls.__instance
