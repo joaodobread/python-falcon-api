97e00b313779dfafdf63c004a9bee1147b38b130
JoaoDoBread <joaovictorslf@gmail.com>
2021-08-01 17:46:27 -0300

    chore: add a correction on database

--- database-schema/dabase.sql
+++ database-schema/dabase.sql
@@ -1,15 +1,15 @@
-CREATE SCHEMA IF NOT EXISTS "stone-api";
-
-CREATE  TABLE customer ( 
+CREATE  TABLE "public".customer ( 
 	user_id              uuid  NOT NULL ,
 	created_at           timestamp DEFAULT current_timestamp NOT NULL ,
 	updated_at           timestamp   ,
 	deleted_at           timestamp   ,
 	geolocation          point  NOT NULL ,
-	CONSTRAINT pk_customer_user_id PRIMARY KEY ( user_id )
+	route_id             uuid  NOT NULL ,
+	CONSTRAINT pk_customer_user_id PRIMARY KEY ( user_id ),
+	CONSTRAINT unq_customer_route_id UNIQUE ( route_id ) 
  );
 
-CREATE  TABLE routes ( 
+CREATE  TABLE "public".routes ( 
 	id                   uuid  NOT NULL ,
 	created_by           uuid  NOT NULL ,
 	bounds               polygon  NOT NULL ,
@@ -17,12 +17,13 @@ CREATE  TABLE routes (
 	created_at           timestamp DEFAULT current_timestamp NOT NULL ,
 	updated_at           timestamp   ,
 	deleted_at           timestamp   ,
+	is_default           boolean DEFAULT false NOT NULL ,
 	CONSTRAINT pk_routes_id PRIMARY KEY ( id ),
 	CONSTRAINT unq_routes_created_by UNIQUE ( created_by ) ,
 	CONSTRAINT unq_routes_selled_id UNIQUE ( selled_id ) 
  );
 
-CREATE  TABLE seller ( 
+CREATE  TABLE "public".seller ( 
 	user_id              uuid  NOT NULL ,
 	email                varchar(300)  NOT NULL ,
 	created_at           timestamp DEFAULT current_timestamp  ,
@@ -31,19 +32,19 @@ CREATE  TABLE seller (
 	CONSTRAINT pk_seller_user_id PRIMARY KEY ( user_id )
  );
 
-CREATE  TABLE tbl ( 
+CREATE  TABLE "public".tbl ( 
  );
 
-CREATE  TABLE user_has_roles ( 
+CREATE  TABLE "public".user_has_roles ( 
 	role_id              uuid  NOT NULL ,
 	user_id              uuid  NOT NULL ,
 	CONSTRAINT unq_user_has_roles_role_id UNIQUE ( role_id ) ,
 	CONSTRAINT unq_user_has_roles_user_id UNIQUE ( user_id ) 
  );
 
-CREATE UNIQUE INDEX unq_user_has_roles ON user_has_roles ( role_id, user_id );
+CREATE UNIQUE INDEX unq_user_has_roles ON "public".user_has_roles ( role_id, user_id );
 
-CREATE  TABLE users ( 
+CREATE  TABLE "public".users ( 
 	id                   uuid  NOT NULL ,
 	name                 varchar(300)  NOT NULL ,
 	created_at           timestamp DEFAULT current_timestamp NOT NULL ,
@@ -52,9 +53,9 @@ CREATE  TABLE users (
 	CONSTRAINT pk_users_id PRIMARY KEY ( id )
  );
 
-CREATE UNIQUE INDEX idx_users ON users ( id, deleted_at );
+CREATE UNIQUE INDEX idx_users ON "public".users ( id, deleted_at );
 
-CREATE  TABLE roles ( 
+CREATE  TABLE "public".roles ( 
 	id                   uuid  NOT NULL ,
 	slug                 varchar(100)  NOT NULL ,
 	description          varchar(500)   ,
@@ -62,15 +63,17 @@ CREATE  TABLE roles (
 	CONSTRAINT pk_roles_id PRIMARY KEY ( id )
  );
 
-ALTER TABLE roles ADD CONSTRAINT fk_roles_user_has_roles FOREIGN KEY ( id ) REFERENCES user_has_roles( role_id );
+ALTER TABLE "public".roles ADD CONSTRAINT fk_roles_user_has_roles FOREIGN KEY ( id ) REFERENCES "public".user_has_roles( role_id );
 
-ALTER TABLE seller ADD CONSTRAINT fk_seller_routes FOREIGN KEY ( user_id ) REFERENCES routes( selled_id );
+ALTER TABLE "public".routes ADD CONSTRAINT fk_routes_customer FOREIGN KEY ( id ) REFERENCES "public".customer( route_id );
 
-ALTER TABLE users ADD CONSTRAINT fk_users_customer FOREIGN KEY ( id ) REFERENCES customer( user_id ) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "public".seller ADD CONSTRAINT fk_seller_routes FOREIGN KEY ( user_id ) REFERENCES "public".routes( selled_id );
 
-ALTER TABLE users ADD CONSTRAINT fk_users_seller FOREIGN KEY ( id ) REFERENCES seller( user_id ) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "public".users ADD CONSTRAINT fk_users_customer FOREIGN KEY ( id ) REFERENCES "public".customer( user_id ) ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE users ADD CONSTRAINT fk_users_user_has_roles FOREIGN KEY ( id ) REFERENCES user_has_roles( user_id );
+ALTER TABLE "public".users ADD CONSTRAINT fk_users_seller FOREIGN KEY ( id ) REFERENCES "public".seller( user_id ) ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE users ADD CONSTRAINT fk_users_routes FOREIGN KEY ( id ) REFERENCES routes( created_by );
+ALTER TABLE "public".users ADD CONSTRAINT fk_users_user_has_roles FOREIGN KEY ( id ) REFERENCES "public".user_has_roles( user_id );
+
+ALTER TABLE "public".users ADD CONSTRAINT fk_users_routes FOREIGN KEY ( id ) REFERENCES "public".routes( created_by );
 
